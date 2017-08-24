be1a030a4dd40e509dcf3a92836c8b3d71eacb7a
XLabour <syarif.hidayatullah@gmail.com>
2017-08-24 16:26:08 +0700

    Update statusfu.php

--- statusfu.php
+++ statusfu.php
@@ -25,11 +25,17 @@ $q = "SELECT count(followup_status) AS totalFU FROM t_registrasi WHERE followup_
 $r = mysqli_query($dblink,$q) or die(mysqli_error($dblink));
 $d = mysqli_fetch_assoc($r);
 
+$q2 = "SELECT count(approvereject_status) AS totalAccept FROM t_registrasi WHERE approvereject_status=2 GROUP BY approvereject_status";
+$r2 = mysqli_query($dblink,$q2) or die(mysqli_error($dblink));
+$d2 = mysqli_fetch_assoc($r2);
+
+
 $arrResult = array(
 	"status"=> true,
 	"msgCode"=> "00",
 	"msg"=>"Success",
-	"totalFollowUp"=>(int) $d['totalFU']
+	"totalFollowUp"=>(int) $d['totalFU'],
+	"totalAccept"=>(int) $d2['totalAccept']
 );
 echo json_encode($arrResult);
 ?>
