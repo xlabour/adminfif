93379d55dc467ebfb820ddca84a29f819696039b
XLabour <syarif.hidayatullah@gmail.com>
2017-08-25 13:30:50 +0700

    Update viewdetail.php

--- viewdetail.php
+++ viewdetail.php
@@ -1,5 +1,5 @@
 <?php
-//error_reporting(0);
+error_reporting(0);
 header("Content-type: application/json");
 session_start();
 if (!isset($_SESSION['username']) || $_SESSION['username']==''){
@@ -18,7 +18,10 @@ $q = "SELECT * FROM v_registrasi WHERE uid='".mysqli_real_escape_string($dblink,
 $r = mysqli_query($dblink,$q) or die(mysqli_error($dblink));
 
 $d = mysqli_fetch_assoc($r);
+$d['jenis_kelamin'] = $d['jenis_kelamin']=='L'?'Laki-laki':'Perempuan';
 $d['lahir_tanggal'] = date("j-F-Y",strtotime($d['lahir_tanggal']));
+$d['dp'] = number_format($d['dp'],0,",",".");
+$d['angsuran'] = number_format($d['angsuran'],0,",",".");
 //print_r($d);
 echo json_encode($d);
 ?>
